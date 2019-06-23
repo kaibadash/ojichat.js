b4b6720708a223e14ad7dc2ce40ec87548dcd7f6
kaiba <kaibadash@gmail.com>
2019-06-23 18:32:56 +0900

    getRandomNameSuffix

--- src/generator/Generator.ts
+++ src/generator/Generator.ts
@@ -5,7 +5,9 @@ import * as _ from 'underscore';
 export class Generator {
     constructor(private target = "", private emojiNum = 4, private punctuationLevel = 0) {
         if (this.target == null || this.target == "") {
-            this.target = this.getRandomFirstName();
+            this.target = this.getRandomFirstName() + this.getRandomNameSuffix();
+        } else {
+            this.target = this.getRandomNameSuffix();
         }
     }
 
@@ -23,5 +25,23 @@ export class Generator {
                 return name.first().katakana();
         }
         return name.first().hiragana();
+    }
+
+    // 「ちゃん」「チャン」などをランダムに返す
+    private getRandomNameSuffix(): string {
+        let n = _.random(100);
+        if (n < 5) {
+            // たまに呼び捨てにするおじさん
+            return ""
+        }
+        if (n < 20) {
+            // 時に「◯◯チャン」とカタカナにしてくるのも、おじさんの常套手段だ。
+            return "チャン"
+        }
+        if (n < 40) {
+            // "「〇〇チャン」をさらに半角で表現する、そんなおじさんもいる"
+            return "ﾁｬﾝ";
+        }
+        return "ちゃん";
     }
 }
